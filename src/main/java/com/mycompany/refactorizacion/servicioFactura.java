ad5aceaa008272815f2a72c5c8b29d4165fbbfdb
Andy Alexander Logacho <logachologacho@gmail.com>
2026-06-26 18:15:00 -0500

    Creaciion de la clase servicioFactura

---
+++ src/main/java/com/mycompany/refactorizacion/servicioFactura.java
@@ -0,0 +1,53 @@
+
+package com.mycompany.refactorizacion;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Andy Alexander
+ */
+public class servicioFactura {
+
+    public void generarFactura(cliente cliente,
+                               List<String> productos,
+                               List<Double> precios,
+                               List<Integer> cantidades,
+                               double subtotal,
+                               double descuento,
+                               double impuesto,
+                               double total) {
+
+        try {
+
+            FileWriter writer =
+                    new FileWriter("factura_" +
+                    cliente.getNombre() + ".txt");
+
+            writer.write("FACTURA\n");
+            writer.write("Cliente: " +
+                         cliente.getNombre() + "\n");
+
+            for (int i = 0; i < productos.size(); i++) {
+
+                writer.write(productos.get(i)
+                        + " x" + cantidades.get(i)
+                        + " = $"
+                        + (precios.get(i) * cantidades.get(i))
+                        + "\n");
+            }
+
+            writer.write("Subtotal: $" + subtotal + "\n");
+            writer.write("Descuento: $" + descuento + "\n");
+            writer.write("Impuesto: $" + impuesto + "\n");
+            writer.write("TOTAL: $" + total);
+
+            writer.close();
+
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
