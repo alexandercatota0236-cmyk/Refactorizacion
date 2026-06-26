e311de70506dcc4f8449213e1d1db8c11b61ff54
Andy Alexander Logacho <logachologacho@gmail.com>
2026-06-26 18:18:07 -0500

    Creaciion de la clase servicioEmail

---
+++ src/main/java/com/mycompany/refactorizacion/servicioEmail.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.refactorizacion;
+
+/**
+ *
+ * @author Andy Alexander
+ */
+public class servicioEmail {
+
+    public void enviarConfirmacion(cliente cliente,
+                                   double total){
+
+        System.out.println("Enviando correo a "
+                + cliente.getEmail());
+
+        System.out.println("Asunto: Confirmacion de pedido");
+
+        System.out.println("Cuerpo: Estimado "
+                + cliente.getNombre()
+                + ", su pedido por $"
+                + total
+                + " ha sido procesado.");
+    }
+
+    public void enviarCancelacion(cliente cliente,
+                                  int idPedido){
+
+        System.out.println("Enviando correo a "
+                + cliente.getEmail());
+
+        System.out.println("Asunto: Cancelacion de pedido");
+
+        System.out.println("Cuerpo: Estimado "
+                + cliente.getNombre()
+                + ", su pedido #"
+                + idPedido
+                + " ha sido cancelado.");
+    }
+
+}
